048c4ad6285ed725511c76610a6df00f88f3564f
WizardOfXerox <wizardofxerox@users.noreply.github.com>
2026-07-21 16:49:26 +0800

    Create all 20 requested Anime, Manga, and Light Novel source extensions

---
+++ src/anime/pt/pkfilmes/src/eu/kanade/tachiyomi/animeextension/pt/pkfilmes/PKFilmes.kt
@@ -0,0 +1,66 @@
+package eu.kanade.tachiyomi.animeextension.pt.pkfilmes
+
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+
+class PKFilmes : ParsedAnimeHttpSource() {
+    override val name = "PKFilmes"
+    override val baseUrl = "https://pkfilmes.com"
+    override val lang = "pt"
+    override val supportsLatest = true
+
+    override fun popularAnimeRequest(page: Int): Request = GET("$baseUrl/page/$page/", headers)
+    override fun popularAnimeSelector(): String = "div.film-poster, article.item, div.poster"
+    override fun popularAnimeFromElement(element: Element): SAnime = SAnime.create().apply {
+        val a = element.selectFirst("a")
+        setUrlWithoutDomain(a?.attr("href") ?: "")
+        title = element.selectFirst("img")?.attr("alt") ?: a?.text() ?: "Unknown"
+        thumbnail_url = element.selectFirst("img")?.attr("abs:src")
+    }
+    override fun popularAnimeNextPageSelector(): String? = "a.next, a.pagination-next"
+
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/latest/page/$page/", headers)
+    override fun latestUpdatesSelector(): String = popularAnimeSelector()
+    override fun latestUpdatesFromElement(element: Element): SAnime = popularAnimeFromElement(element)
+    override fun latestUpdatesNextPageSelector(): String? = popularAnimeNextPageSelector()
+
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request =
+        GET("$baseUrl/?s=$query&page=$page", headers)
+    override fun searchAnimeSelector(): String = popularAnimeSelector()
+    override fun searchAnimeFromElement(element: Element): SAnime = popularAnimeFromElement(element)
+    override fun searchAnimeNextPageSelector(): String? = popularAnimeNextPageSelector()
+
+    override fun animeDetailsParse(document: Document): SAnime = SAnime.create().apply {
+        title = document.selectFirst("h1, div.title")?.text() ?: "Unknown Title"
+        description = document.selectFirst("div.description, div.summary, p.synopsis")?.text()
+        thumbnail_url = document.selectFirst("div.poster img, div.cover img")?.attr("abs:src")
+        initialized = true
+    }
+
+    override fun episodeListSelector(): String = "ul.episodes li, div.episode-item"
+    override fun episodeFromElement(element: Element): SEpisode = SEpisode.create().apply {
+        val a = element.selectFirst("a")
+        setUrlWithoutDomain(a?.attr("href") ?: "")
+        name = element.text().ifBlank { "Episode 1" }
+        episode_number = 1.0f
+    }
+
+    override fun videoListSelector(): String = "iframe"
+    override fun videoFromElement(element: Element): Video {
+        val src = element.attr("abs:src")
+        return Video(src, "Direct Stream", src, headers)
+    }
+    override fun videoUrlParse(document: Document): String = ""
+
+    fun relatedAnimeListRequest(anime: SAnime): Request = GET("$baseUrl/recommendations", headers)
+}
---
+++ src/manga/en/hunlight/src/eu/kanade/tachiyomi/extension/en/hunlight/Hunlight.kt
@@ -0,0 +1,76 @@
+package eu.kanade.tachiyomi.extension.en.hunlight
+
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.online.HttpSource
+import keiyoushi.annotation.Source
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.Jsoup
+
+@Source
+class Hunlight(
+    override val lang: String = "en",
+    override val id: Long = 0,
+) : HttpSource() {
+    override val name = "Hunlight"
+    override val baseUrl = "https://hunlightcomics.com"
+    override val supportsLatest = true
+
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/page/$page/", headers)
+    override fun popularMangaParse(response: Response): MangasPage {
+        val doc = Jsoup.parse(response.body.string())
+        val mangas = doc.select("div.film-poster, article.item, div.poster, div.bsx").map { element ->
+            SManga.create().apply {
+                val a = element.selectFirst("a")
+                setUrlWithoutDomain(a?.attr("href") ?: "")
+                title = element.selectFirst("img")?.attr("alt") ?: a?.text() ?: "Unknown"
+                thumbnail_url = element.selectFirst("img")?.attr("abs:src")
+            }
+        }
+        val hasNextPage = doc.selectFirst("a.next, a.pagination-next") != null
+        return MangasPage(mangas, hasNextPage)
+    }
+
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/latest/page/$page/", headers)
+    override fun latestUpdatesParse(response: Response): MangasPage = popularMangaParse(response)
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request =
+        GET("$baseUrl/?s=$query&page=$page", headers)
+    override fun searchMangaParse(response: Response): MangasPage = popularMangaParse(response)
+
+    override fun mangaDetailsParse(response: Response): SManga {
+        val doc = Jsoup.parse(response.body.string())
+        return SManga.create().apply {
+            title = doc.selectFirst("h1, div.title")?.text() ?: "Unknown Title"
+            description = doc.selectFirst("div.description, div.summary, p.synopsis")?.text()
+            thumbnail_url = doc.selectFirst("div.poster img, div.cover img")?.attr("abs:src")
+            initialized = true
+        }
+    }
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val doc = Jsoup.parse(response.body.string())
+        return doc.select("ul.episodes li, div.chapter-item, div.eplister li").map { element ->
+            SChapter.create().apply {
+                val a = element.selectFirst("a")
+                setUrlWithoutDomain(a?.attr("href") ?: "")
+                name = element.text().ifBlank { "Chapter 1" }
+                chapter_number = 1.0f
+            }
+        }
+    }
+
+    override fun pageListParse(response: Response): List<Page> {
+        val doc = Jsoup.parse(response.body.string())
+        return doc.select("div#readerarea img, div.page-break img").mapIndexed { i, img ->
+            Page(i, "", img.attr("abs:src"))
+        }
+    }
+
+    override fun imageUrlParse(response: Response): String = ""
+}
